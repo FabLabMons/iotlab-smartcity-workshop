4dec412b845615e1576f90c46017440f1c7f2584
François Roland <fr.roland@gmail.com>
2018-09-07 22:47:41 +0200

    Write distance capture code

---
+++ iotlab-capture-distance/iotlab-capture-distance.ino
@@ -0,0 +1,32 @@
+static const uint8_t sensorTriggerPin = 3;
+static const uint8_t sensorEchoPin = 4;
+
+static uint8_t distanceCm = 255;
+
+const unsigned TX_INTERVAL = 3;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Starting"));
+
+  pinMode(sensorTriggerPin, OUTPUT);
+  pinMode(sensorEchoPin, INPUT);
+
+}
+
+void loop() {
+  digitalWrite(sensorTriggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(sensorTriggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensorTriggerPin, LOW);
+
+  long duration = pulseIn(sensorEchoPin, HIGH);
+  distanceCm = duration * 0.034 / 2;
+
+  Serial.print(F("Distance: "));
+  Serial.print(duration);
+  Serial.println("cm");
+
+  delay(TX_INTERVAL * 1000);
+}
